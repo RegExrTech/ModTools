9b754332432f7a0118634400dbcd684e1c01750d
Devon Wasson <devonwasson@gmail.com>
2019-11-21 14:19:16 +0000

    removed references to hardcoded removal reasons

--- report.py
+++ report.py
@@ -1,5 +1,4 @@
 import time
-import removal_reasons
 from collections import defaultdict
 
 debug = False
@@ -16,18 +15,14 @@ def remove_reported_posts(sub, sub_name):
                 if item.mod_reports:
                         report_reason = item.mod_reports[0][0]
 			mod_name = item.mod_reports[0][1]
-                        try:
-                                message = removal_reasons.removal_reasons[report_reason]['message']
-                                title = removal_reasons.removal_reasons[report_reason]['title']
-                        except Exception as e:  # If we were not able to get this information, it means that there is no removal reason set up and I should fix that
-				message = ""
-				title = report_reason
-				for rule in sub.rules()['rules']:
-					if rule['short_name'] == report_reason:
-						message = rule['description']
-				if not message:
-	                                messasge = "Your post has been removed."
-				message = "\n\n" + message + "\n\n---\n\nIf you have any questions or can make changes to your post that would allow it to be approved, please reply to this message.\n\n---\n\n"
+			message = ""
+			title = report_reason
+			for rule in sub.rules()['rules']:
+				if rule['short_name'] == report_reason:
+					message = rule['description']
+			if not message:
+	                               messasge = "Your post has been removed."
+			message = "\n\n" + message + "\n\n---\n\nIf you have any questions or can make changes to your post that would allow it to be approved, please reply to this message.\n\n---\n\n"
 
 			try:
 	                        item.mod.remove()
@@ -46,9 +41,10 @@ def remove_reported_posts(sub, sub_name):
 					ids_to_mods[title].append(mod_name)
 				except Exception as e:
 					if i == 2:
-						print("Unable to send removal reason.")
+						print("Unable to send removal reason for sub " + sub_name + ".")
 						print(e)
 					else:
+						print("Failed to send removal message: " + message + ". Sleeping for 3 seconds and trying again...")
 						time.sleep(3)
 	return ids_to_mods
 
