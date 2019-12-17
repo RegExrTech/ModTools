f0e92952fe9e40ee5021d61f6527cd1e5a1f77db
Devon Wasson <devonwasson@gmail.com>
2019-12-17 20:36:00 +0000

    report now includes the submission text in the removal reason. Also, large refactor of that file

--- report.py
+++ report.py
@@ -3,48 +3,70 @@ from collections import defaultdict
 
 debug = False
 
-def remove_reported_posts(sub, sub_name):
-	ids_to_mods = defaultdict(lambda: [])
+def get_reports(sub, sub_name):
 	try:
-		reports = sub.mod.reports()
-	except Exception as e:
+		return sub.mod.reports()
+	except:
 		print("Unable to read reports for " + sub_name)
 		print(e)
-		return
-        for item in reports:
-                if item.mod_reports:
-                        report_reason = item.mod_reports[0][0]
-			mod_name = item.mod_reports[0][1]
-			message = ""
-			title = report_reason
-			for rule in sub.rules()['rules']:
-				if rule['short_name'] == report_reason:
-					message = rule['description']
-			if not message:
-	                               messasge = "Your post has been removed."
-			message = "\n\n" + message + "\n\n---\n\nIf you have any questions or can make changes to your post that would allow it to be approved, please reply to this message.\n\n---\n\n"
+		return []
 
-			try:
-	                        item.mod.remove()
-			except Exception as e:
-				print("Unable to remove post.")
+def get_rule_text(report_reason, sub):
+	for rule in sub.rules()['rules']:
+		if rule['short_name'] == report_reason:
+			return "\n\n" + rule['description']
+	return "\n\nYour post has been removed."
+
+def get_submission_text(item):
+	try:  # Text post
+		return "Title: " + item.title + "\n\nBody: " + item.selftext
+	except:
+		try:  # Link Post
+			return "Title: " + item.title + "\n\nLink: " + item.url
+		except:  # Comment
+			return "Comment: " + item.body
+
+def remove_post(item):
+	try:
+		item.mod.remove()
+	except:
+		print("Unable to remove post.")
+		print(e)
+		return False
+	return True
+
+def send_removal_reason(item, message, title, mod_name, ids_to_mods):
+	removal_reason_sent = False
+	for i in range(3):  # Take three attempts at sending removal reason
+		if removal_reason_sent:
+			break
+		try:
+			item.mod.send_removal_message(message, title=title, type='private')
+			removal_reason_sent = True
+			ids_to_mods[title].append(mod_name)
+		except Exception as e:
+			if i == 2:
+				print("Unable to send removal reason for sub " + sub_name + ".")
 				print(e)
-				continue
-			# Take three attempts at sending removal reason
-			removal_reason_sent = False
-			for i in range(3):
-				if removal_reason_sent:
-					break
-				try:
-		                        item.mod.send_removal_message(message, title=title, type='private')
-					removal_reason_sent = True
-					ids_to_mods[title].append(mod_name)
-				except Exception as e:
-					if i == 2:
-						print("Unable to send removal reason for sub " + sub_name + ".")
-						print(e)
-					else:
-						print("Failed to send removal message: " + message + ". Sleeping for 3 seconds and trying again...")
-						time.sleep(3)
+			else:
+				print("Failed to send removal message: " + message + ". Sleeping for 3 seconds and trying again...")
+				time.sleep(3)
+
+def remove_reported_posts(sub, sub_name):
+	ids_to_mods = defaultdict(lambda: [])
+        for item in get_reports(sub, sub_name):
+                if not item.mod_reports:
+			continue
+		report_reason = item.mod_reports[0][0]
+		title = report_reason
+		message = get_rule_text(report_reason, sub)
+		submission_text = get_submission_text(item)
+
+		message += "\n\n---\n\nSubmission:\n\n" + submission_text
+		message +=  "\n\n---\n\nIf you have any questions or can make changes to your post that would allow it to be approved, please reply to this message.\n\n---\n\n"
+
+		if remove_post(item):
+			send_removal_reason(item, message, title, item.mod_reports[0][1], ids_to_mods)
+
 	return ids_to_mods
 
