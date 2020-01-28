baf57adb02229036af4a4cddc946858726cd2df4
Devon Wasson <devonwasson@gmail.com>
2020-01-28 16:34:21 +0000

    added a nice tool for generating reports based on sub removals

---
+++ generate_report.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from collections import defaultdict
+
+cutoff_date = datetime.strptime("2020-01-01", "%Y-%m-%d")
+sub_name = "funkoswap"
+report_file_name = "database/report_log-" + sub_name + ".txt"
+
+f = open(report_file_name, 'r')
+lines = f.read().splitlines()
+f.close()
+
+reasons = defaultdict(lambda: 0)
+mods = defaultdict(lambda: 0)
+for line in lines:
+	items = line.split(" - ")
+	date = datetime.strptime(items[0], "%Y-%m-%d")
+	mod = items[1]
+	reason = items[2]
+
+	if date < cutoff_date:
+		continue
+
+	mods[mod] += 1
+	reasons[reason] += 1
+
+
+print("\n".join([str(x[0]) + " - " + str(x[1]) for x in sorted(mods.items(), key=lambda x: x[1], reverse=True)]))
+print("\n=====\n")
+print("\n".join([str(x[0]) + " - " + str(x[1]) for x in sorted(reasons.items(), key=lambda x: x[1], reverse=True)]))
