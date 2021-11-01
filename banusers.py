d9372f992e07f1e922f84c954c32066119e68dfc
NateNate60 <nathanlim2004@gmail.com>
2021-11-01 16:25:01 +0000

    Add module to propagate bans

---
+++ banusers.py
@@ -0,0 +1,36 @@
+"""
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import praw
+
+def getbanned (r: praw.Reddit, subreddit: str) -> list :
+    """
+    Fetch banned users from a subreddit.
+
+    Due to a Reddit API limitation this will fetch at most 100 users.
+    """
+    bannedlist = []
+    for user in r.subreddit(subreddit).banned() :
+        bannedlist.append(user.name)
+    return bannedlist
+
+def banusers (r : praw.Reddit, list: list, subreddit: str) -> None :
+    """
+    Mass-ban a list of users from a subreddit. Skips users that are already banned.
+    """
+    for user in list :
+        if user not in r.subreddit(subreddit).banned() :
+            r.subreddit('c4crep').banned.add(user)
+            print ("Banned", user)
