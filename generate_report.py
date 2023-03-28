ab1a0aa0f8113dc838b97f1ddebea27759a07e4c
Devon Wasson <devonwasson@gmail.com>
2023-03-28 02:18:29 +0000

    adds more report options to the report generator

--- generate_report.py
+++ generate_report.py
@@ -1,11 +1,8 @@
 from datetime import datetime
 from collections import defaultdict
 
-cutoff_date = datetime.strptime("2019-10-01", "%Y-%m-%d")
-#sub_name = "pkmntcgtrades"
-sub_name= 'funkoswap'
+cutoff_date = datetime.strptime("2023-01-01", "%Y-%m-%d")
 sub_name = "Watchexchange"
-#sub_name = "funkopop"
 report_file_name = "database/report_log-" + sub_name + ".txt"
 
 f = open(report_file_name, 'r')
@@ -14,6 +11,7 @@ f.close()
 
 reasons = defaultdict(lambda: 0)
 mods = defaultdict(lambda: 0)
+mods_to_reasons = defaultdict(lambda: defaultdict(lambda: 0))
 for line in lines:
 	items = line.split(" - ")
 	date = datetime.strptime(items[0], "%Y-%m-%d")
@@ -26,9 +24,14 @@ for line in lines:
 	print(str(date) + " - " + mod + " - " + reason)
 	mods[mod] += 1
 	reasons[reason] += 1
+	mods_to_reasons[mod][reason] += 1
 
 print("\nTotal removals: " + str(sum([mods[x] for x in mods])))
 print("\n=====\n")
 print("\n".join([str(x[0]) + " - " + str(x[1]) for x in sorted(mods.items(), key=lambda x: x[1], reverse=True)]))
 print("\n=====\n")
 print("\n".join([str(x[0]) + " - " + str(x[1]) for x in sorted(reasons.items(), key=lambda x: x[1], reverse=True)]))
+print("\n=====\n")
+for mod in mods_to_reasons:
+	print("=== " + mod + " ===")
+	print("    " + "\n    ".join([str(x[0]) + " - " + str(x[1]) for x in sorted(mods_to_reasons[mod].items(), key=lambda x: x[1], reverse=True)]))
