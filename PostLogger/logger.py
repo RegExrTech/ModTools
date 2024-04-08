b2f4e3dcad3982cc27689d43370689b661d0e75b
Devon Wasson <devonwasson@gmail.com>
2024-04-08 18:22:23 +0000

    move PostLogger/logger.py to use the new Config format

--- PostLogger/logger.py
+++ PostLogger/logger.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.insert(0, ".")
 import string
 import praw
 import json
@@ -5,29 +7,12 @@ import math
 import argparse
 import os
 import time
+import Config
 
 parser = argparse.ArgumentParser()
-parser.add_argument('config_file_name', metavar='C', type=str)
+parser.add_argument('sub_name', metavar='C', type=str)
 args = parser.parse_args()
-config_fname = 'config/' + args.config_file_name
-if not os.path.exists("config"):
-        os.mkdir("config")
-
-f = open(config_fname, "r")
-info = f.read().splitlines()
-f.close()
-
-config = {}
-for item in info:
-        pair = item.split(":")
-        config[pair[0]] = pair[1]
-
-subreddit_name = config['subreddit_name']
-client_id = config['client_id']
-client_secret = config['client_secret']
-bot_username = config['bot_username']
-bot_password = config['bot_password']
-refresh_token = config['refresh_token']
+CONFIG = Config.Config(args.sub_name.lower())
 
 debug = False
 
@@ -50,12 +35,10 @@ def dump(db, fname):
                 outfile.write(str(db).replace("'", '"').replace('{u"', '{"').replace(' u"', ' "').encode('ascii','ignore'))
 
 def main():
-	reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent='Post Logging Bot for ' + subreddit_name + ' v1.0 (by u/RegExr)', refresh_token=refresh_token)
-	sub = reddit.subreddit(subreddit_name)
 	db = get_db(db_fname)
-	if subreddit_name not in db:
-		db[subreddit_name] = {}
-	posts = sub.new(limit=20)
+	if CONFIG.subreddit_name not in db:
+		db[CONFIG.subreddit_name] = {}
+	posts = CONFIG.subreddit.new(limit=20)
 	for post in posts:
 		updated = False
 		author = post.author.name.lower()
@@ -64,19 +47,19 @@ def main():
 		title = ''.join(filter(lambda x: x in set(string.printable), title))
 		title = title.replace("'", "").replace('"', "")
 		id = post.id
-		if author not in db[subreddit_name] or not db[subreddit_name][author]:
-			db[subreddit_name][author] = [{"id": id, "created": created, "title": title}]
+		if author not in db[CONFIG.subreddit_name] or not db[CONFIG.subreddit_name][author]:
+			db[CONFIG.subreddit_name][author] = [{"id": id, "created": created, "title": title}]
 			updated = True
-		if created > db[subreddit_name][author][-1]["created"]:
-			db[subreddit_name][author].append({"id": id, "created": created, "title": title})
+		if created > db[CONFIG.subreddit_name][author][-1]["created"]:
+			db[CONFIG.subreddit_name][author].append({"id": id, "created": created, "title": title})
 			updated = True
-		for historic_post in db[subreddit_name][author][::]:
+		for historic_post in db[CONFIG.subreddit_name][author][::]:
 			if historic_post["created"] + (7*24*60*60) < current_time:
-				db[subreddit_name][author].remove(historic_post)
+				db[CONFIG.subreddit_name][author].remove(historic_post)
 				updated = True
 		if updated:
-			wiki_text = "\n\n".join(["[" + x["title"] + "](https://redd.it/" + x["id"] + ") - " + time.ctime(x["created"]) for x in db[subreddit_name][author]])
-			page = sub.wiki['user_post_history/'+author]
+			wiki_text = "\n\n".join(["[" + x["title"] + "](https://redd.it/" + x["id"] + ") - " + time.ctime(x["created"]) for x in db[CONFIG.subreddit_name][author]])
+			page = CONFIG.subreddit.wiki['user_post_history/'+author]
 			page.edit(wiki_text)
 	dump(db, db_fname)
 
