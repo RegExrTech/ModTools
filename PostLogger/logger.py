59ac733d1455a91d223654920958ada4d5310b4c
Devon Wasson <devonwasson@gmail.com>
2025-08-20 19:00:32 +0000

    update post logger to save databases to specific files rather than one large file

--- PostLogger/logger.py
+++ PostLogger/logger.py
@@ -17,22 +17,20 @@ CONFIG = Config.Config(args.sub_name.lower())
 debug = False
 
 current_time = time.time()
-db_fname = "PostLogger/posts.json"
+db_fname = "PostLogger/posts/{}_posts.json".format(args.sub_name.lower())
 
-# required function for getting ASCII from json load
-def ascii_encode_dict(data):
-        ascii_encode = lambda x: x.encode('ascii') if isinstance(x, unicode) else x
-        return dict(map(ascii_encode, pair) for pair in data.items())
-
-# Function to load the DB into memory
 def get_db(fname):
-        with open(fname) as json_data: # open the funko-shop's data
-                data = json.load(json_data, object_hook=ascii_encode_dict)
+	if not os.path.exists(fname):
+		f = open(fname, "w")
+		f.write("{}")
+		f.close()
+        with open(fname) as json_data:
+                data = json.load(json_data)
         return data
 
 def dump(db, fname):
-        with open(fname, 'w') as outfile:  # Write out new data
-                outfile.write(str(db).replace("'", '"').replace('{u"', '{"').replace(' u"', ' "').encode('ascii','ignore'))
+        with open(fname, 'w') as outfile:
+		outfile.write(json.dumps(db, sort_keys=True, indent=4))
 
 def main():
 	db = get_db(db_fname)
